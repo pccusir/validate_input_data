ed687cf1f669af2d67be6a21d8383a0cc58f665b
pccusir <wfx2@ulive.pccu.edu.tw>
2024-04-28 23:08:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def form():
-    return render_template('form.html')
+    return render_template('input_data.html')
 
 @app.route('/submit_form', methods=['POST'])
 def handle_form():
