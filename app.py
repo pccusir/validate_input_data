7ca484c4f40ab158c5ba5aeaa8841b3e5da70e1b
pccusir <wfx2@ulive.pccu.edu.tw>
2024-04-29 10:48:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def handle_form():
     return "All entries are valid", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)  # Listen on all available network interfaces and port 80
+
